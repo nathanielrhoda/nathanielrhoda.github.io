b2ea763fabab79c0f2aef7d6b1d3da974d58f357
Nathaniel <nathanielrhoda@gmail.com>
2016-03-03 11:48:49 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//$(document).ready(function() {
+  //$("#copyright").click(function(){ 
+  	//$(this).css('color','purple'); 
+	//});
+//});
+
+document.getElementById('copyright').onclick = function () { 
+   this.style.color = 'purple';
+}
